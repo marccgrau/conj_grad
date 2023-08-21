734676ec0a1fc1f8441230f067a53f9791b5e82b
marccgrau <46031196+marccgrau@users.noreply.github.com>
2023-08-21 09:03:08 +0200

    added flat models

--- src/models/flat_cnn.py
+++ src/models/flat_cnn.py
@@ -1,34 +1,25 @@
 import tensorflow as tf
 
 
-# TODO: Netz verkleinern
-class BasicCNN(tf.keras.Model):
+class FlatCNN(tf.keras.Model):
     def __init__(self, num_classes, **kwargs):
-        super(BasicCNN, self).__init__(**kwargs)
+        super(FlatCNN, self).__init__(**kwargs)
         self.conv1 = tf.keras.layers.Conv2D(
             filters=3,
             kernel_size=(3, 3),
             strides=1,
             padding="same",
         )
-        # self.bn1 = tf.keras.layers.BatchNormalization()
+        self.pool1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))
         self.conv2 = tf.keras.layers.Conv2D(
             filters=6,
             kernel_size=(3, 3),
             strides=1,
             padding="same",
         )
-        # self.bn2 = tf.keras.layers.BatchNormalization()
-        self.conv3 = tf.keras.layers.Conv2D(
-            filters=9,
-            kernel_size=(3, 3),
-            strides=1,
-            padding="same",
-        )
-        # self.bn3 = tf.keras.layers.BatchNormalization()
+        self.pool2 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))
         self.flatten = tf.keras.layers.Flatten()
-        self.fc1 = tf.keras.layers.Dense(units=8, activation="relu")
-        self.fc2 = tf.keras.layers.Dense(units=num_classes, activation="softmax")
+        self.fc1 = tf.keras.layers.Dense(units=num_classes, activation="softmax")
 
     def train_step(self, data):
         # fetch data
@@ -41,16 +32,12 @@ class BasicCNN(tf.keras.Model):
 
     def call(self, inputs, training=None, **kwargs):
         x = self.conv1(inputs)
-        # x = self.bn1(x, training=training)
+        x = self.pool1(x)
         x = tf.nn.relu(x)
         x = self.conv2(x)
-        # x = self.bn2(x, training=training)
-        x = tf.nn.relu(x)
-        x = self.conv3(x)
-        # x = self.bn3(x, training=training)
+        x = self.pool2(x)
         x = tf.nn.relu(x)
         x = self.flatten(x)
-        x = self.fc1(x)
-        output = self.fc2(x)
+        output = self.fc1(x)
 
         return output
---
+++ src/models/flat_mlp.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+
+class FlatMLP(tf.keras.Model):
+    def __init__(self, num_classes, **kwargs):
+        super(FlatMLP, self).__init__(**kwargs)
+        self.flatten = tf.keras.layers.Flatten()
+        self.hidden = tf.keras.layers.Dense(units=128, activation="relu")
+        self.output_layer = tf.keras.layers.Dense(
+            units=num_classes, activation="softmax"
+        )
+
+    def train_step(self, data):
+        # fetch data
+        x, y = data
+        # apply updates from optimizer
+        tempmodel = self.optimizer.apply_gradients(self.trainable_variables, x, y)
+        self.set_weights(tempmodel.get_weights())
+        self.compiled_metrics.update_state(y, self(x))
+        return {m.name: m.result() for m in self.metrics}
+
+    def call(self, inputs):
+        x = self.flatten(inputs)
+        x = self.hidden(x)
+        output = self.output_layer(x)
+        return output
