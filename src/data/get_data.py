adb3c6048dd1957c5d1707d28e78ab88ecd4fdb8
marccgrau <46031196+marccgrau@users.noreply.github.com>
2023-08-21 09:51:53 +0200

    add fashion mnist

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -1,13 +1,6 @@
-from pathlib import Path
 from typing import Optional
-import joblib
-import numpy as np
 import tensorflow as tf
-from tensorflow.keras.utils import get_file
-
-from tensorflow.keras.datasets import mnist
 import tensorflow_datasets as tfds
-
 from src.configs.configs import DataConfig
 
 keras = tf.keras
@@ -17,95 +10,102 @@ _IMAGE_SIZE_IMAGENET = (224, 224)
 _IMAGE_SIZE_CIFAR = (32, 32)
 _IMAGE_SIZE_MNIST = (28, 28)
 
-def _load_imagenet(data_config: DataConfig): 
+
+def _load_imagenet(data_config: DataConfig):
     train_ds, test_ds = tfds.load(
-        "imagenet2012", 
-        split=["train", "validation"], 
-        shuffle_files=True, 
-        ) 
-    # Imagenet dataset has arbitrary sizes, we need to resize all of them. 
-    resizer = tf.keras.layers.Resizing(*_IMAGE_SIZE_IMAGENET) 
-    
-    def resize(img, label): 
-        return resizer(img), label 
-    
+        "imagenet2012",
+        split=["train", "validation"],
+        shuffle_files=True,
+    )
+    # Imagenet dataset has arbitrary sizes, we need to resize all of them.
+    resizer = tf.keras.layers.Resizing(*_IMAGE_SIZE_IMAGENET)
+
+    def resize(img, label):
+        return resizer(img), label
+
     def one_hot(image, label):
         # Casts to an Int and performs one-hot ops
         label = tf.one_hot(tf.cast(label, tf.int32), data_config.num_classes)
         # Recasts it to Float32
         label = tf.cast(label, tf.float32)
         return image, label
-    
-    def prepare(split): 
-        split = split.map( lambda item: (item["image"], item["label"]), num_parallel_calls=tf.data.AUTOTUNE, ) 
-        split = split.map(resize, num_parallel_calls=tf.data.AUTOTUNE) 
-        split = split.map(one_hot, num_parallel_calls=tf.data.AUTOTUNE) 
-        return split 
-    
-    train_ds = prepare(train_ds) 
-    test_ds = prepare(test_ds) 
+
+    def prepare(split):
+        split = split.map(
+            lambda item: (item["image"], item["label"]),
+            num_parallel_calls=tf.data.AUTOTUNE,
+        )
+        split = split.map(resize, num_parallel_calls=tf.data.AUTOTUNE)
+        split = split.map(one_hot, num_parallel_calls=tf.data.AUTOTUNE)
+        return split
+
+    train_ds = prepare(train_ds)
+    test_ds = prepare(test_ds)
     return train_ds, test_ds
 
-def _load_cifar10(data_config: DataConfig): 
+
+def _load_cifar10(data_config: DataConfig):
     train_ds, test_ds = tfds.load(
-        "cifar10", 
-        split=["train", "test"], 
+        "cifar10",
+        split=["train", "test"],
         data_dir=data_config.path,
-        shuffle_files=True, 
+        shuffle_files=True,
         as_supervised=True,
-        ) 
-    # ensure all images have same size 
-    resizer = tf.keras.layers.Resizing(*_IMAGE_SIZE_CIFAR) 
-    
-    def resize(img, label): 
+    )
+    # ensure all images have same size
+    resizer = tf.keras.layers.Resizing(*_IMAGE_SIZE_CIFAR)
+
+    def resize(img, label):
         img /= 255
-        return resizer(img), label 
-    
+        return resizer(img), label
+
     def one_hot(image, label):
         # Casts to an Int and performs one-hot ops
         label = tf.one_hot(tf.cast(label, tf.int32), data_config.num_classes)
         # Recasts it to Float32
         label = tf.cast(label, tf.float32)
         return image, label
-    
-    def prepare(split): 
-        split = split.map(resize, num_parallel_calls=tf.data.AUTOTUNE) 
-        split = split.map(one_hot, num_parallel_calls=tf.data.AUTOTUNE) 
-        return split 
-    
-    train_ds = prepare(train_ds) 
-    test_ds = prepare(test_ds) 
+
+    def prepare(split):
+        split = split.map(resize, num_parallel_calls=tf.data.AUTOTUNE)
+        split = split.map(one_hot, num_parallel_calls=tf.data.AUTOTUNE)
+        return split
+
+    train_ds = prepare(train_ds)
+    test_ds = prepare(test_ds)
     return train_ds, test_ds
 
-def _load_cifar100(data_config: DataConfig): 
+
+def _load_cifar100(data_config: DataConfig):
     train_ds, test_ds = tfds.load(
-        "cifar100", 
-        split=["train", "test"], 
-        shuffle_files=True, 
+        "cifar100",
+        split=["train", "test"],
+        shuffle_files=True,
         as_supervised=True,
-        ) 
-    # ensure all images have same size 
-    resizer = tf.keras.layers.Resizing(*_IMAGE_SIZE_CIFAR) 
-    
-    def resize(img, label): 
+    )
+    # ensure all images have same size
+    resizer = tf.keras.layers.Resizing(*_IMAGE_SIZE_CIFAR)
+
+    def resize(img, label):
         img /= 255
-        return resizer(img), label 
-    
+        return resizer(img), label
+
     def one_hot(image, label):
         # Casts to an Int and performs one-hot ops
         label = tf.one_hot(tf.cast(label, tf.int32), data_config.num_classes)
         # Recasts it to Float32
         label = tf.cast(label, tf.float32)
         return image, label
-    
-    def prepare(split): 
-        split = split.map(resize, num_parallel_calls=tf.data.AUTOTUNE) 
-        split = split.map(one_hot, num_parallel_calls=tf.data.AUTOTUNE) 
-        return split 
-    
-    train_ds = prepare(train_ds) 
-    test_ds = prepare(test_ds) 
+
+    def prepare(split):
+        split = split.map(resize, num_parallel_calls=tf.data.AUTOTUNE)
+        split = split.map(one_hot, num_parallel_calls=tf.data.AUTOTUNE)
+        return split
+
+    train_ds = prepare(train_ds)
+    test_ds = prepare(test_ds)
     return train_ds, test_ds
+
 
 def _load_mnist(data_config: DataConfig):
     """
@@ -113,39 +113,76 @@ def _load_mnist(data_config: DataConfig):
     @param data_config:
     @return: train_ds, test_ds
     """
-    path = data_config.path
     train_ds, test_ds = tfds.load(
-        "mnist", 
-        split=["train", "test"], 
-        shuffle_files=True, 
+        "mnist",
+        split=["train", "test"],
+        shuffle_files=True,
         as_supervised=True,
-        )
-    # ensure all images have same size 
-    resizer = tf.keras.layers.Resizing(*_IMAGE_SIZE_MNIST) 
-    
-    def resize(img, label): 
+    )
+    # ensure all images have same size
+    resizer = tf.keras.layers.Resizing(*_IMAGE_SIZE_MNIST)
+
+    def resize(img, label):
         img /= 255
         return resizer(img), label
-    
+
     def one_hot(image, label):
         # Casts to an Int and performs one-hot ops
         label = tf.one_hot(tf.cast(label, tf.int32), data_config.num_classes)
         # Recasts it to Float32
         label = tf.cast(label, tf.float32)
         return image, label
-    
-    def prepare(split):  
-        split = split.map(resize, num_parallel_calls=tf.data.AUTOTUNE) 
-        split = split.map(one_hot, num_parallel_calls=tf.data.AUTOTUNE) 
-        return split 
-    
-    train_ds = prepare(train_ds) 
-    test_ds = prepare(test_ds) 
-    
+
+    def prepare(split):
+        split = split.map(resize, num_parallel_calls=tf.data.AUTOTUNE)
+        split = split.map(one_hot, num_parallel_calls=tf.data.AUTOTUNE)
+        return split
+
+    train_ds = prepare(train_ds)
+    test_ds = prepare(test_ds)
+
     return train_ds, test_ds
 
+
+def _load_fashion_mnist(data_config: DataConfig):
+    """
+    Loads the Fashion-MNIST dataset.
+    @param data_config:
+    @return: train_ds, test_ds
+    """
+    train_ds, test_ds = tfds.load(
+        "fashion_mnist",
+        split=["train", "test"],
+        shuffle_files=True,
+        as_supervised=True,
+    )
+    # ensure all images have same size
+    resizer = tf.keras.layers.Resizing(*_IMAGE_SIZE_MNIST)
+
+    def resize(img, label):
+        img /= 255
+        return resizer(img), label
+
+    def one_hot(image, label):
+        # Casts to an Int and performs one-hot ops
+        label = tf.one_hot(tf.cast(label, tf.int32), data_config.num_classes)
+        # Recasts it to Float32
+        label = tf.cast(label, tf.float32)
+        return image, label
+
+    def prepare(split):
+        split = split.map(resize, num_parallel_calls=tf.data.AUTOTUNE)
+        split = split.map(one_hot, num_parallel_calls=tf.data.AUTOTUNE)
+        return split
+
+    train_ds = prepare(train_ds)
+    test_ds = prepare(test_ds)
+
+    return train_ds, test_ds
+
+
 def fetch_data(
-    data_config: DataConfig
+    data_config: DataConfig,
 ) -> tuple[tf.data.Dataset, Optional[tf.data.Dataset]]:
     """
     Returns the reference/training data from file.
@@ -156,16 +193,21 @@ def fetch_data(
     """
     data_config.path.mkdir(parents=True, exist_ok=True)
 
-    if "MNIST" in data_config.name:
+    lowercase_name = data_config.name.lower()
+
+    if lowercase_name == "mnist":
         ds_train, ds_test = _load_mnist(data_config)
         return ds_train, ds_test
-    elif "IMAGENET" in data_config.name:
+    elif lowercase_name == "fashion_mnist":
+        ds_train, ds_test = _load_fashion_mnist(data_config)
+        return ds_train, ds_test
+    elif lowercase_name == "imagenet":
         ds_train, ds_test = _load_imagenet(data_config)
         return ds_train, ds_test
-    elif "CIFAR100" in data_config.name:
+    elif lowercase_name == "cifar100":
         ds_train, ds_test = _load_cifar100(data_config)
         return ds_train, ds_test
-    elif "CIFAR10" in data_config.name:
+    elif lowercase_name == "cifar10":
         ds_train, ds_test = _load_cifar10(data_config)
         return ds_train, ds_test
     else:
