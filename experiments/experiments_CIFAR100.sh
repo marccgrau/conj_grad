60b3bd4ca461a7241c7da8d1eb0b03fe6967f3a2
marccgrau <46031196+marccgrau@users.noreply.github.com>
2023-09-05 11:19:22 +0200

    max calls for experiment schedule

--- experiments/experiments_CIFAR100.sh
+++ experiments/experiments_CIFAR100.sh
@@ -1,25 +1,25 @@
-python main.py --path "/home/user/code/data/tf_data" --model FlatCNNCifar100 --model_size small --data CIFAR100 --dtype float64 --optimizer RMSPROP --batch_size 75 --gpu 9 --run_eagerly --max_epochs 43 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatCNNCifar100 --model_size small --data CIFAR100 --dtype float64 --optimizer RMSPROP --batch_size 2500 --gpu 8 --run_eagerly --max_epochs 500000 --use_wandb --max_calls 58289
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatCNNCifar100 --model_size large --data CIFAR100 --dtype float64 --optimizer RMSPROP --batch_size 75 --gpu 9 --run_eagerly --max_epochs 38 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatCNNCifar100 --model_size large --data CIFAR100 --dtype float64 --optimizer RMSPROP --batch_size 2500 --gpu 8 --run_eagerly --max_epochs 500000 --use_wandb --max_calls 50818
 
-#python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size small --data CIFAR100 --dtype float64 --optimizer RMSPROP --batch_size 75 --gpu 9 --run_eagerly --max_epochs 24 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size small --data CIFAR100 --dtype float64 --optimizer RMSPROP --batch_size 2500 --gpu 8 --run_eagerly --max_epochs 500000 --use_wandb --max_calls 33378
 
-#python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size large --data CIFAR100 --dtype float64 --optimizer RMSPROP --batch_size 75 --gpu 9 --run_eagerly --max_epochs 24 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size large --data CIFAR100 --dtype float64 --optimizer RMSPROP --batch_size 2500 --gpu 8 --run_eagerly --max_epochs 500000 --use_wandb --max_calls 32517
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatCNNCifar100 --model_size small --data CIFAR100 --dtype float64 --optimizer ADAM --batch_size 75 --gpu 9 --run_eagerly --max_epochs 43 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatCNNCifar100 --model_size small --data CIFAR100 --dtype float64 --optimizer ADAM --batch_size 2500 --gpu 8 --run_eagerly --max_epochs 500000 --use_wandb --max_calls 58289
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatCNNCifar100 --model_size large --data CIFAR100 --dtype float64 --optimizer ADAM --batch_size 75 --gpu 9 --run_eagerly --max_epochs 38 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatCNNCifar100 --model_size large --data CIFAR100 --dtype float64 --optimizer ADAM --batch_size 2500 --gpu 8 --run_eagerly --max_epochs 500000 --use_wandb --max_calls 50818
 
-#python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size small --data CIFAR100 --dtype float64 --optimizer ADAM --batch_size 75 --gpu 9 --run_eagerly --max_epochs 24 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size small --data CIFAR100 --dtype float64 --optimizer ADAM --batch_size 2500 --gpu 8 --run_eagerly --max_epochs 500000 --use_wandb --max_calls 33378
 
-#python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size large --data CIFAR100 --dtype float64 --optimizer ADAM --batch_size 75 --gpu 9 --run_eagerly --max_epochs 24 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size large --data CIFAR100 --dtype float64 --optimizer ADAM --batch_size 2500 --gpu 8 --run_eagerly --max_epochs 500000 --use_wandb --max_calls 32517
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatCNNCifar100 --model_size small --data CIFAR100 --dtype float64 --optimizer SGD --batch_size 75 --gpu 9 --run_eagerly --max_epochs 43 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatCNNCifar100 --model_size small --data CIFAR100 --dtype float64 --optimizer SGD --batch_size 2500 --gpu 8 --run_eagerly --max_epochs 500000 --use_wandb --max_calls 58289
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatCNNCifar100 --model_size large --data CIFAR100 --dtype float64 --optimizer SGD --batch_size 75 --gpu 9 --run_eagerly --max_epochs 38 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatCNNCifar100 --model_size large --data CIFAR100 --dtype float64 --optimizer SGD --batch_size 2500 --gpu 8 --run_eagerly --max_epochs 500000 --use_wandb --max_calls 50818
 
-#python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size small --data CIFAR100 --dtype float64 --optimizer SGD --batch_size 75 --gpu 9 --run_eagerly --max_epochs 24 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size small --data CIFAR100 --dtype float64 --optimizer SGD --batch_size 2500 --gpu 8 --run_eagerly --max_epochs 500000 --use_wandb --max_calls 33378
 
-#python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size large --data CIFAR100 --dtype float64 --optimizer SGD --batch_size 75 --gpu 9 --run_eagerly --max_epochs 24 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size large --data CIFAR100 --dtype float64 --optimizer SGD --batch_size 2500 --gpu 8 --run_eagerly --max_epochs 500000 --use_wandb --max_calls 32517
 
 
--- experiments/experiments_sgd.sh
+++ experiments/experiments_sgd.sh
@@ -1,24 +1,23 @@
-python main.py --path "/home/user/code/data/tf_data" --model FlatCNN --model_size small --data MNIST --dtype float64 --optimizer SGD --batch_size 50 --gpu 9 --run_eagerly --max_epochs 3 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatCNN --model_size small --data MNIST --dtype float64 --optimizer SGD --batch_size 3000 --gpu 9 --run_eagerly --max_epochs 3 --use_wandb --max_calls 10985
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatCNN --model_size large --data MNIST --dtype float64 --optimizer SGD --batch_size 50 --gpu 9 --run_eagerly --max_epochs 3 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatCNN --model_size large --data MNIST --dtype float64 --optimizer SGD --batch_size 3000 --gpu 9 --run_eagerly --max_epochs 3 --use_wandb --max_calls 10985
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size small --data MNIST --dtype float64 --optimizer SGD --batch_size 50 --gpu 9 --run_eagerly --max_epochs 3 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size small --datdataa MNIST --dtype float64 --optimizer SGD --batch_size 6000 --gpu 9 --run_eagerly --max_epochs 50000 --use_wandb --max_calls 7545
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size large --data MNIST --dtype float64 --optimizer SGD --batch_size 50 --gpu 9 --run_eagerly --max_epochs 3 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size large --data MNIST --dtype float64 --optimizer SGD --batch_size 6000 --gpu 9 --run_eagerly --max_epochs 50000 --use_wandb --max_calls 6426
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatCNN --model_size small --data FASHION_MNIST --dtype float64 --optimizer SGD --batch_size 75 --gpu 9 --run_eagerly --max_epochs 15 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatCNN --model_size small --data FASHION_MNIST --dtype float64 --optimizer SGD --batch_size 3000 --gpu 9 --run_eagerly --max_epochs 50000 --use_wandb --max_calls 25293
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatCNN --model_size large --data FASHION_MNIST --dtype float64 --optimizer SGD --batch_size 75 --gpu 9 --run_eagerly --max_epochs 15 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatCNN --model_size large --data FASHION_MNIST --dtype float64 --optimizer SGD --batch_size 3000 --gpu 9 --run_eagerly --max_epochs 50000 --use_wandb --max_calls 23236
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size small --data FASHION_MNIST --dtype float64 --optimizer SGD --batch_size 75 --gpu 9 --run_eagerly --max_epochs 7 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size small --data FASHION_MNIST --dtype float64 --optimizer SGD --batch_size 6000 --gpu 9 --run_eagerly --max_epochs 50000 --use_wandb --max_calls 9519
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size large --data FASHION_MNIST --dtype float64 --optimizer SGD --batch_size 75 --gpu 9 --run_eagerly --max_epochs 7 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size large --data FASHION_MNIST --dtype float64 --optimizer SGD --batch_size 6000 --gpu 9 --run_eagerly --max_epochs 50000 --use_wandb --max_calls 9500
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatCNN --model_size small --data CIFAR10 --dtype float64 --optimizer SGD --batch_size 75 --gpu 9 --run_eagerly --max_epochs 20 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatCNN --model_size small --data CIFAR10 --dtype float64 --optimizer SGD --batch_size 2500 --gpu 9 --run_eagerly --max_epochs 50000 --use_wandb --max_calls 26454
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatCNN --model_size large --data CIFAR10 --dtype float64 --optimizer SGD --batch_size 75 --gpu 9 --run_eagerly --max_epochs 20 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatCNN --model_size large --data CIFAR10 --dtype float64 --optimizer SGD --batch_size 2500 --gpu 9 --run_eagerly --max_epochs 50000 --use_wandb --max_calls 23651
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size small --data CIFAR10 --dtype float64 --optimizer SGD --batch_size 75 --gpu 9 --run_eagerly --max_epochs 7 --use_wandb
+python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size small --data CIFAR10 --dtype float64 --optimizer SGD --batch_size 5000 --gpu 9 --run_eagerly --max_epochs 50000 --use_wandb --max_calls 9452
 
-python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size large --data CIFAR10 --dtype float64 --optimizer SGD --batch_size 75 --gpu 9 --run_eagerly --max_epochs 7 --use_wandb
-
+python main.py --path "/home/user/code/data/tf_data" --model FlatMLP --model_size large --data CIFAR10 --dtype float64 --optimizer SGD --batch_size 5000 --gpu 9 --run_eagerly --max_epochs 50000  --use_wandb --max_calls 8094
